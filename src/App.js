b426efde4221875ff57e32054fdd4eefee834c96
hhq <13486485935@163.com>
2024-12-20 19:07:43 +0800

    mod: 初始化2.1

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import 'dayjs/locale/zh-cn';
 // 由于 antd 组件的默认文案是英文，所以需要修改为中文
 import zhCN from 'antd/locale/zh_CN';
 
+import Layout from "./layout";
 import Home from './views/home'
+import Login from './views/login'
+import Test from './views/test'
 
 dayjs.locale('zh-cn');
 
@@ -16,6 +19,10 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="home" element={<Home />} />
+          <Route element={<Layout />}>
+            <Route path="login" element={<Login />} />
+            <Route path="test" element={<Test />} />
+          </Route>
         </Routes>
       </div>
     </ConfigProvider>
---
+++ src/layout/index.jsx
@@ -0,0 +1,7 @@
+import { Outlet } from "react-router";
+export default function Layout() {
+  return <div>
+    123
+    <Outlet />
+  </div>
+}
